5bc87a198568b4429ff8719e3e0ba56becae14ed
Justin McIntyre <justinmcintyre42@gmail.com>
2025-10-25 22:34:53 -0300

    added character import export skills for pyfa

--- src/routes/corporations/CorporationCard.svelte
+++ src/routes/corporations/CorporationCard.svelte
@@ -7,21 +7,23 @@
     let { corporation } = $props()
     
     const membersQuery = await supabase
-        .schema('esi').from('CharacterSharedInfo').select('id')
+        .schema('esi').from('CharacterSharedInfo').select('id,skills')
         .eq('corporation_id', $corporation.data.corporation_id)
 </script>
 {#if !$corporation.loading}
-<div class="m-5 bg-slate-600 rounded-xl p-4">
-    <div class="flex flex-horizontal items-center">
-        <div><CorporationIcon size={64} corporation_id={$corporation.data.corporation_id} /></div>
+<div class="m-5 bg-neutral-900 rounded-lg p-2">
+    <div class="flex flex-horizontal items-center bg-neutral-800 rounded m-2">
+        <div class="rounded overflow-clip m-4"><CorporationIcon size={64} corporation_id={$corporation.data.corporation_id} /></div>
         <div class="h-10 p-2">{$corporation.data.name}</div>
     </div>
-    {#if membersQuery.error}
-        <pre>{JSON.stringify(membersQuery.error, null, 2)}</pre>
-    {:else}
-        {#each membersQuery.data as memberData }
-            <MembersCard memberData={memberData} />            
-        {/each} 
-    {/if}
+    <div class="mt-4 m-2 flex gap-8 flex-wrap">
+        {#if membersQuery.error}
+            <pre>{JSON.stringify(membersQuery.error, null, 2)}</pre>
+        {:else}
+            {#each membersQuery.data as memberData }
+                <MembersCard memberData={memberData} />    
+            {/each} 
+        {/if}
+    </div>
 </div>
 {/if}
--- src/routes/corporations/MembersCard.svelte
+++ src/routes/corporations/MembersCard.svelte
@@ -1,11 +1,31 @@
 <script>
 	import Portrait from "$lib/components/Portrait.svelte";
+	import { convertToPyfaFormat } from "$lib/staticInfo/pyfaSkillExport";
+	import { skillIdToName } from "$lib/staticInfo/skillsIdNameMap";
+	import { characterPublicInfoStore } from "$lib/stores/characterPublicInfo";
 
     let { memberData } = $props()
 
+    let characterInfoSlice = characterPublicInfoStore.select({ character_id: memberData.id })
+
+async function exportClipToClipError() {
+    let pyfaFormat = convertToPyfaFormat(memberData.skills)
+    const type = "text/plain";
+    const clipboardItemData = {
+        [type]: pyfaFormat,
+    };
+    const clipboardItem = new ClipboardItem(clipboardItemData);
+    await navigator.clipboard.write([clipboardItem]);
+    console.log(pyfaFormat)
+}
 </script>
 
-<div class="flex flex-horizontal items-center">
-    <div><Portrait character_id={memberData.id} size={64} /></div>
-    <div class="h-10 p-2">name</div>
+<div class="p-4 bg-neutral-800 rounded hover:bg-neutral-700">
+    <div class="flex flex-horizontal items-center">
+        <div class="rounded overflow-clip"><Portrait character_id={memberData.id} size={64} /></div>
+        <div class="h-10 p-2">{$characterInfoSlice.data?.name}</div>
+    </div>
+    <button onclick={exportClipToClipError} class="bg-indigo-600/30 cursor-pointer mt-4 hover:bg-indigo-500/30 border-indigo-500 p-4 w-xs rounded">
+        Export skill to pyfa
+    </button>
 </div>
